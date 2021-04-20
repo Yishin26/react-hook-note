7f1c1b518180639503661f4d9fa6371b5dd380a0
Louise Lin <yishinlin@outlook.com>
2021-04-20 13:01:56 +0800

    useEffect 接入維基百科API

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+  console.log(results);
   useEffect(() => {
-    console.log("只跑第一次");
-  }, []); //傳入空值時只跑第一次
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term
+        }
+      });
+      if (term !== "") {
+        setResults(data.query.search);
+      }
+    };
+
+    search();
+  }, [term]); //傳入空值時只跑第一次
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -17,6 +47,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
